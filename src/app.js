f380083d213d3b70552d34b5b0acd82925bd0ff9
Bruno Pache <bruno_x86@hotmail.com>
2024-03-07 14:03:44 -0300

    correcciones y cookies

--- src/app.js
+++ src/app.js
@@ -7,11 +7,7 @@ import ProductRouter from "./routes/products.router.js";
 import CartRouter from "./routes/carts.router.js";
 import ViewsRouter from "./routes/views.router.js";
 import cookieParser from "cookie-parser";
-import session from "express-session";
-import FileStore from "session-file-store";
 import "../src/database.js";
-
-const fileStore = FileStore(session);
 
 const app = express();
 const PUERTO = 8080;
@@ -32,15 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: "secretcoder",
-    resave: true,
-    saveUninitialized: true,
-
-    store: new fileStore({ path: "./src/sessions", ttl: 100, retries: 1 }),
-  })
-);
 
 //Routes
 app.use("/api/products", ProductRouter);
@@ -57,17 +44,4 @@ app.get("/createcookie", (req, res) => {
 
 app.get("/deletecookie", (req, res) => {
   res.clearCookie("cookie server").send("Cookie Deleted");
-});
-
-app.get("/login", (req, res) => {
-  let user = req.query.user;
-  req.session.user = user;
-  res.send("User saved");
-});
-
-app.get("/user", (req, res) => {
-  if (req.session.user) {
-    return res.send(`User logged: ${req.session.user}`);
-  }
-  res.send("User Not Found");
 });
