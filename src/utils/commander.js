09f8f790cd3a49ba02912f956c11973f8544be61
Bruno Pache <bruno_x86@hotmail.com>
2024-04-16 10:10:50 -0300

    Arquitectura por capas

--- src/utils/commander.js
+++ src/utils/commander.js
@@ -1,7 +1,11 @@
 import { Command } from "commander";
 const program = new Command();
 
-program.option("--mode <mode>", "modo de trabajo", "produccion");
+program
+  .option("-d", "Variable para debug", false)
+  .option("-p <port>", "puerto del servidor", 8080)
+  .option("--mode <mode>", "modo de trabajo", "produccion");
+
 program.parse();
 
 export default program;
